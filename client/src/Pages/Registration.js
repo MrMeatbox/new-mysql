351bb83b96a51c368571a6fbb4504656d841faeb
MrMeatbox <86042319+MrMeatbox@users.noreply.github.com>
2021-08-08 00:41:48 +0600

    Add files via upload

---
+++ client/src/Pages/Registration.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+function Registration() {
+  const initialValues = {
+    username: "",
+    password: "",
+    usermail:"",
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(3).max(15).required(),
+    password: Yup.string().min(4).max(20).required(),
+    usermail:Yup.string().min(4).max(20).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/auth", data).then(() => {
+      console.log(data);
+    });
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Username: </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="username"
+            placeholder="(Ex. John123...)"
+          />
+
+          <label>Password: </label>
+          <ErrorMessage name="password" component="span" />
+          <Field
+            autocomplete="off"
+            type="password"
+            id="inputCreatePost"
+            name="password"
+            placeholder="Your Password..."
+          />
+           <label>UserMail: </label>
+          <ErrorMessage name="usermail" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="usermail"
+            placeholder="(Ex. John123@gmail.com...)"
+          />
+
+          <button type="submit"> Register</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default Registration;
